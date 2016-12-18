14d46b9ccb66396ca4082af2f2139fe9ad29b09a
SG3D\Adam Campbell <adam@sg3d.com.au>
2016-12-18 16:00:20 +1000

    Updated pricing on CreateOrderViewModel. Also added a payment button for you to check out and investigate.

--- SGFastFlyers/ViewModels/CreateOrderViewModel.cs
+++ SGFastFlyers/ViewModels/CreateOrderViewModel.cs
@@ -113,7 +113,8 @@ namespace SGFastFlyers.ViewModels
         /// Price logic currently resides here.
         /// <para>
         /// TODO:
-        ///     Work out a more centralized way to calculate cost.
+        ///     Work out a more centralized way to calculate cost. The formula below works for the time being
+        ///     ....needs better input fields for updating.
         ///         -Won't have to update it across both views and controllers when the logic changes
         /// </para>
         /// </summary>
@@ -121,47 +122,117 @@ namespace SGFastFlyers.ViewModels
         {
             get
             {
-                decimal cost = (decimal)this.Quantity / 1000 * Config.BaseCostPer1000() ?? -1;
-                  
-                if (!this.IsMetro)
+                decimal quantity = Quantity / 1000;
+                decimal cost = 0;
+                decimal dlSingleSided = 0;
+                decimal dlDoubleSided = 0;
+                decimal A5SingleSided = 0;
+                decimal A5DoubleSided = 0;
+
+                if (quantity >= 1 && quantity < 20)
+                {
+                    cost = 50;
+                    dlSingleSided = 40;
+                    dlDoubleSided = 43;
+                    A5SingleSided = 52;
+                    A5DoubleSided = 55.9m;
+                }
+                if (quantity >= 20 && quantity < 50)
+                {
+                    cost = 49;
+                    dlSingleSided = 38;
+                    dlDoubleSided = 41;
+                    A5SingleSided = 49.4m;
+                    A5DoubleSided = 53.3m;
+                }
+                if (quantity >= 50 && quantity < 75)
+                {
+                    cost = 48;
+                    dlSingleSided = 36;
+                    dlDoubleSided = 41;
+                    A5SingleSided = 46.8m;
+                    A5DoubleSided = 53.3m;
+                }
+                if (quantity >= 75 && quantity <= 100)
+                {
+                    cost = 47;
+                    dlSingleSided = 36;
+                    dlDoubleSided = 41;
+                    A5SingleSided = 46.8m;
+                    A5DoubleSided = 53.3m;
+                }
+                if (quantity >= 100 && quantity <= 200)
+                {
+                    cost = 46;
+                    dlSingleSided = 34;
+                    dlDoubleSided = 38;
+                    A5SingleSided = 44.2m;
+                    A5DoubleSided = 49.4m;
+                }
+                if (quantity >= 200 && quantity < 300)
+                {
+                    cost = 45;
+                    dlSingleSided = 34;
+                    dlDoubleSided = 38;
+                    A5SingleSided = 44.2m;
+                    A5DoubleSided = 49.4m;
+                }
+                if (quantity >= 300)
+                {
+                    cost = 42;
+                    dlSingleSided = 32;
+                    dlDoubleSided = 36;
+                    A5SingleSided = 41.6m;
+                    A5DoubleSided = 46.8m;
+                }
+                cost = cost * quantity;
+                if (IsMetro == false)
                 {
                     cost = cost + (decimal)Config.NonMetroAddition();
                 }
 
-                if (this.NeedsPrint)
-                {
-                    if (this.IsDoubleSided)
-                    {
-                        if (this.PrintSize == Enums.PrintSize.DL)
-                        {
-                            cost = cost + ((decimal)this.Quantity / 1000 * (decimal)Config.DLDoubleSidedPer1000());
-                        }
 
-                        if (this.PrintSize == Enums.PrintSize.A5)
+
+                if (NeedsPrint)
+
+
+                {
+                    if (IsDoubleSided)
+
+                    {
+
+                        if (PrintSize == Enums.PrintSize.DL)
+
                         {
-                            cost = cost + ((decimal)this.Quantity / 1000 * (decimal)Config.A5DoubleSidedPer1000());
+                            cost = cost + dlDoubleSided * quantity;
+                        }
+                        if (PrintSize == Enums.PrintSize.A5)
+                        {
+                            cost = cost + A5DoubleSided * quantity;
                         }
                     }
                     else
                     {
-                        if (this.PrintSize == Enums.PrintSize.DL)
+
+                        if (PrintSize == Enums.PrintSize.DL)
                         {
-                            cost = cost + ((decimal)this.Quantity / 1000 * (decimal)Config.DLSingleSidedPer1000());
+                            cost = cost + dlSingleSided * quantity;
                         }
 
-                        if (this.PrintSize == Enums.PrintSize.A5)
+                        if (PrintSize == Enums.PrintSize.A5)
                         {
-                            cost = cost + ((decimal)this.Quantity / 1000 * (decimal)Config.A5SingleSidedPer1000());
+                            cost = cost + A5SingleSided * quantity;
                         }
                     }
                 }
-
                 if (cost < 400)
                 {
                     cost = 400;
-                } 
+                }
 
-                return cost;               
+
+                return cost;
+
             }
 
             set
