4ec9ba1fe5451fefa91ecca1ea5c4208738787c4
glenncampbell <glenn@gen3media.com>
2017-01-23 19:22:50 +1000

    https

--- SGFastFlyers/Global.asax.cs
+++ SGFastFlyers/Global.asax.cs
@@ -14,6 +14,7 @@ namespace SGFastFlyers
     {
         protected void Application_Start()
         {
+            GlobalFilters.Filters.Add(new RequireHttpsAttribute());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
