cb6203a0e3cc552be3c4908d97c5f754c95899ec
sanjaysvarma <sanjay19@hotmail.com>
2017-02-02 15:11:07 +0800

    -Testimonials files and integration on Home page. Sanjay S Varma

--- SGFastFlyers/App_Start/BundleConfig.cs
+++ SGFastFlyers/App_Start/BundleConfig.cs
@@ -14,6 +14,14 @@ namespace SGFastFlyers
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryowl").Include(
+                        "~/Scripts/jquery.js?ver=1.12.3",
+                        "~/Scripts/jquery.easing.js",
+                        "~/Scripts/jquery.jcarousel.min.js",
+                        "~/Scripts/Custom/global.js",
+                        "~/Scripts/owl.carousel.min.js"
+                        ));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -24,9 +32,14 @@ namespace SGFastFlyers
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
                       "~/Content/bootstrap-theme.css",
                       "~/Content/site.css",
-                      "~/Content/circle.css"));
+                      "~/Content/circle.css",
+                      "~/Content/owl.carousel.css",
+                      "~/Content/owl.theme.css"
+                      ));
+
             //Create bundel for jQueryUI  
             //js  
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
---
+++ SGFastFlyers/Scripts/Custom/global.js
@@ -0,0 +1,74 @@
+﻿
+jQuery(document).ready(function ($) {
+    'use strict';
+    $(function () {
+        $("#testimonial-slide").owlCarousel({
+            loop: true,
+            margin: 20,
+            navigation: false,
+            pagination: true,
+            items: 2,
+            itemsDesktop: [1199, 2],
+            itemsDesktopSmall: [980, 2],
+            itemsTablet: [768, 2],
+            itemsTabletSmall: false,
+            itemsMobile: [479, 1]
+
+        });
+
+
+    });
+
+    var jcarousel = $('.jcarousel');
+
+    jcarousel
+        .on('jcarousel:reload jcarousel:create', function () {
+            var width = jcarousel.innerWidth();
+
+            if (width >= 992) {
+                width = width / 5;
+            } else if (width >= 768) {
+                width = width / 4;
+            } else if (width >= 480) {
+                width = width / 3;
+            } else if (width >= 350) {
+                width = width / 2;
+            }
+
+            jcarousel.jcarousel('items').css('width', width + 'px');
+        })
+        .jcarousel({
+            wrap: 'circular'
+        });
+
+    $('.jcarousel-control-prev')
+        .jcarouselControl({
+            target: '-=1'
+        });
+
+    $('.jcarousel-control-next')
+        .jcarouselControl({
+            target: '+=1'
+        });
+
+    $('.jcarousel-pagination')
+        .on('jcarouselpagination:active', 'a', function () {
+            $(this).addClass('active');
+        })
+        .on('jcarouselpagination:inactive', 'a', function () {
+            $(this).removeClass('active');
+        })
+        .on('click', function (e) {
+            e.preventDefault();
+        })
+        .jcarouselPagination({
+            perPage: 1,
+            item: function (page) {
+                return '<a href="#' + page + '">' + page + '</a>';
+            }
+        });
+
+
+
+});
+
